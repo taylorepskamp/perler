f10878ec037d72762473a2ac44bfd32fe7762271
Taylor Epskamp <taylorepskamp@gmail.com>
2020-10-21 08:54:59 -0700

    Updates

---
+++ precache-manifest.2cea9c469a106b6c9dfd2b0ec6a0e6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577754da88f0c7524fc5f4d0d274f90e",
+    "url": "/perler/index.html"
+  },
+  {
+    "revision": "83f80853fa1d7e1b72b9",
+    "url": "/perler/static/css/main.24526a03.chunk.css"
+  },
+  {
+    "revision": "6966a159bfcce4535b6a",
+    "url": "/perler/static/js/2.e929c0ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/perler/static/js/2.e929c0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f80853fa1d7e1b72b9",
+    "url": "/perler/static/js/main.d804b125.chunk.js"
+  },
+  {
+    "revision": "04f3bcf3cc8a70f51433",
+    "url": "/perler/static/js/runtime-main.7a833a4b.js"
+  }
+]);
